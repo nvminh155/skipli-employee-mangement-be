8d3ad133fb62898fd6d19ab4e2436e1f5602d907
nvminh155 <minhnv155@gmail.com>
2025-09-17 22:03:25 +0700

    config firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -7,9 +7,9 @@ const {getFirestore} = require("firebase/firestore");
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "skipli-employee-management.firebaseapp.com",
-  projectId: "skipli-employee-management",
-  storageBucket: "skipli-employee-management.firebasestorage.app",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 };
