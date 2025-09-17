4ddbc073bef14cce14ba07bcef85948b4d05ea08
nvminh155 <minhnv155@gmail.com>
2025-09-17 22:09:01 +0700

    update config & folder structure

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 var express = require("express");
+const { default: config } = require("./src/config");
 var app = express();
 
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(config.port, () => {
+ console.log(`Server running on port ${config.port}`);
 });
