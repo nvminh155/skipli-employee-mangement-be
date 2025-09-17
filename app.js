0a1b4c1ce865518068cec531c545eba6b1e108ee
nvminh155 <minhnv155@gmail.com>
2025-09-17 22:18:30 +0700

    setup route

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ dotenv.config();
 
 var express = require("express");
 const { default: config } = require("./src/config");
-var app = express();
+const { routerV1 } = require("./src/routes/v1");
+const app = express();
 
+
+app.use(express.json());
+
+app.use("/api/v1", routerV1);
 
 app.listen(config.port, () => {
  console.log(`Server running on port ${config.port}`);
---
+++ src/routes/v1/index.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+module.exports = { routerV1: router };
