1fc802cc0acafd22af37b2398612332c50f67b6e
nvminh155 <minhnv155@gmail.com>
2025-09-20 14:40:26 +0700

    feat: update auth middleware to protect route

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+const { db } = require("../firebase");
+const { doc, getDoc } = require("firebase/firestore");
 
 module.exports = async (req, res, next) => {
   try {
@@ -8,13 +10,18 @@ module.exports = async (req, res, next) => {
     const token = authHeader.split(" ")[1];
     if (!token) throw new Error("Unauthorized");
 
-    const data = jwt.verify(token, process.env.JWT_SECRET)
-    
-    const user = await userModel.getUser(data.id);
-    req.user = decoded;
-    
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    if (!data || Date.now() >= data.exp * 1000) {
+      throw new Error("Unauthorized");
+    }
+
+    const userRef = doc(db, "users", data.id);
+    const user = await getDoc(userRef);
+    req.user = user.data();
+
+
     next();
   } catch (err) {
     res.status(401).json({ error: "Unauthorized", success: false });
   }
-}
+};
