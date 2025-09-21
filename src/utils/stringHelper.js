4c769566e45ec35fc2869cc8bd4e87b3edaf0cb9
nvminh155 <minhnv155@gmail.com>
2025-09-21 20:30:05 +0700

    feat: compare string with includes

--- src/utils/stringHelper.js
+++ src/utils/stringHelper.js
@@ -2,6 +2,11 @@ function compareIgnoreCase(a, b) {
   return a.toLowerCase() === b.toLowerCase();
 }
 
+function includeIgnoreCase(a, b) {
+  return a.toLowerCase().includes(b.toLowerCase());
+}
+
 module.exports = {
   compareIgnoreCase,
+  includeIgnoreCase,
 };
