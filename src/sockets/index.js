3af672b39ddea512392533699aeb1be586bc93e5
nvminh155 <minhnv155@gmail.com>
2025-09-21 23:09:54 +0700

    feat: conversation

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -4,9 +4,7 @@ const chatChannel = require("./chat.channel");
 module.exports = function (io) {
 
   io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
     socket.on("disconnect", (socket) => {
-      console.log("a user disconnected", socket.id);
     });
   });
 
@@ -19,7 +17,6 @@ module.exports = function (io) {
       socket.join(room.id);
     });
 
-    // console.log("yours rooms = ", rooms)
     socket.emit("rooms", rooms);
     chatChannel(socket, io.of("/chat"));
   });
